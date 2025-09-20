3a7dc0a303db7c25cafe9228f98d0716606d014a
Suwat Srichan <exadsdev@gmail.com>
2025-09-20 15:35:49 +0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -68,7 +68,8 @@ export default function RootLayout({ children }) {
               `}
             </Script>
           </>
-        )}
+        )} 
+<meta name="google-adsense-account" content="ca-pub-7060960935275939"/>
       </head>
 
       <body>
